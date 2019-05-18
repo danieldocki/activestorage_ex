dc1a1fe2903652288ec78e7da352199abe1e29df
Kieran Eglin <kieran.eglin@gmail.com>
2019-05-18 11:48:35 -0700

    Added elixir_ls to gitignore

--- lib/activestorage_ex.ex
+++ lib/activestorage_ex.ex
@@ -1,18 +1,3 @@
 defmodule ActivestorageEx do
-  @moduledoc """
-  Documentation for ActivestorageEx.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ActivestorageEx.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/activestorage_ex_test.exs
+++ test/activestorage_ex_test.exs
@@ -1,8 +1,4 @@
 defmodule ActivestorageExTest do
   use ExUnit.Case
   doctest ActivestorageEx
-
-  test "greets the world" do
-    assert ActivestorageEx.hello() == :world
-  end
 end
