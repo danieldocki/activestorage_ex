4246061e091bc793ccab19922c58d182d4c72d3c
Kieran Eglin <kieran.eglin@gmail.com>
2019-05-18 11:40:32 -0700

    Added seeds file and activestorage migration

--- test/activestorage_ex_rails/db/seeds.rb
+++ test/activestorage_ex_rails/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+
+10.times do
+  user = User.new
+
+  file_url = "http://robohash.org/#{SecureRandom.hex}.png"
+  remote_file = Net::HTTP.get_response(URI.parse(file_url))
+  user.avatar.attach(
+    io: StringIO.new(remote_file.body), 
+    filename: "#{SecureRandom.hex}.png", 
+    content_type: 'image/png'
+  )
+
+  user.save
+end
