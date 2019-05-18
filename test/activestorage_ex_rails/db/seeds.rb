8775ee5de563d9f4bbb3ab0ac28a8ebced852a8c
Kieran Eglin <kieran.eglin@gmail.com>
2019-05-18 11:46:34 -0700

    Added Post scaffold for testing many attachments

--- test/activestorage_ex_rails/db/seeds.rb
+++ test/activestorage_ex_rails/db/seeds.rb
@@ -13,3 +13,19 @@ require 'net/http'
 
   user.save
 end
+
+10.times do
+  post = Post.new
+
+  3.times do 
+    file_url = "http://robohash.org/#{SecureRandom.hex}.png"
+    remote_file = Net::HTTP.get_response(URI.parse(file_url))
+    post.photos.attach(
+      io: StringIO.new(remote_file.body), 
+      filename: "#{SecureRandom.hex}.png", 
+      content_type: 'image/png'
+    )
+  end
+
+  post.save
+end
