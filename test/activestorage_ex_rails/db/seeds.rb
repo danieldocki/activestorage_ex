79bb2e0591392eec24906ada4a314e43915a467c
Kieran Eglin <kieran.eglin@gmail.com>
2019-05-18 12:47:43 -0700

    Updated seeds file to use better image host

--- test/activestorage_ex_rails/db/seeds.rb
+++ test/activestorage_ex_rails/db/seeds.rb
@@ -3,12 +3,12 @@ require 'net/http'
 10.times do
   user = User.new
 
-  file_url = "http://robohash.org/#{SecureRandom.hex}.png"
+  file_url = 'http://lorempixel.com/300/300/'
   remote_file = Net::HTTP.get_response(URI.parse(file_url))
   user.avatar.attach(
     io: StringIO.new(remote_file.body), 
-    filename: "#{SecureRandom.hex}.png", 
-    content_type: 'image/png'
+    filename: "#{SecureRandom.hex}.jpeg", 
+    content_type: 'image/jpeg'
   )
 
   user.save
@@ -18,12 +18,12 @@ end
   post = Post.new
 
   3.times do 
-    file_url = "http://robohash.org/#{SecureRandom.hex}.png"
+    file_url = 'http://lorempixel.com/300/300/'
     remote_file = Net::HTTP.get_response(URI.parse(file_url))
     post.photos.attach(
       io: StringIO.new(remote_file.body), 
-      filename: "#{SecureRandom.hex}.png", 
-      content_type: 'image/png'
+      filename: "#{SecureRandom.hex}.jpeg", 
+      content_type: 'image/jpeg'
     )
   end
 
